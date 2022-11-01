35f2c90de3a88d989412a447d606212867abbd19
dillanmay <dillanmay_6@yahoo.com>
2022-11-01 08:17:27 -0700

    prior to merge

--- api/utils.js
+++ api/utils.js
@@ -9,9 +9,9 @@ function requireUser(req, res, next) {
     next();
   }
   
-  someRouter.post('/some/route', requireUser, async (req, res, next) => {
+//   someRouter.post('/some/route', requireUser, async (req, res, next) => {
 
-});
+// });
 
 
   module.exports = {
