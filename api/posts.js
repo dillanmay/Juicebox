7c32ace158ae0c3e44aeaef114db66588087ee00
irfan <ipekusic@yahoo.com>
2022-11-01 11:42:24 -0400

    routes tested, and works

--- api/posts.js
+++ api/posts.js
@@ -47,40 +47,40 @@ postsRouter.get('/', async (req, res) => {
     });
   });
 
-  postsRouter.patch('/:postId', requireUser, async (req, res, next) => {
-    const { postId } = req.params;
-    const { title, content, tags } = req.body;
+  // postsRouter.patch('/:postId', requireUser, async (req, res, next) => {
+  //   const { postId } = req.params;
+  //   const { title, content, tags } = req.body;
   
-    const updateFields = {};
+  //   const updateFields = {};
   
-    if (tags && tags.length > 0) {
-      updateFields.tags = tags.trim().split(/\s+/);
-    }
+  //   if (tags && tags.length > 0) {
+  //     updateFields.tags = tags.trim().split(/\s+/);
+  //   }
   
-    if (title) {
-      updateFields.title = title;
-    }
+  //   if (title) {
+  //     updateFields.title = title;
+  //   }
   
-    if (content) {
-      updateFields.content = content;
-    }
+  //   if (content) {
+  //     updateFields.content = content;
+  //   }
   
-    try {
-      const originalPost = await getPostById(postId);
+  //   try {
+  //     const originalPost = await getPostById(postId);
   
-      if (originalPost.author.id === req.user.id) {
-        const updatedPost = await updatePost(postId, updateFields);
-        res.send({ post: updatedPost })
-      } else {
-        next({
-          name: 'UnauthorizedUserError',
-          message: 'You cannot update a post that is not yours'
-        })
-      }
-    } catch ({ name, message }) {
-      next({ name, message });
-    }
-  });
+  //     if (originalPost.author.id === req.user.id) {
+  //       const updatedPost = await updatePost(postId, updateFields);
+  //       res.send({ post: updatedPost })
+  //     } else {
+  //       next({
+  //         name: 'UnauthorizedUserError',
+  //         message: 'You cannot update a post that is not yours'
+  //       })
+  //     }
+  //   } catch ({ name, message }) {
+  //     next({ name, message });
+  //   }
+  // });
 
   
 
